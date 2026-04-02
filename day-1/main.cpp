42b0f610d6b938e6a8056b63fb887d3331bbf863
Unicorn7141 <harelibr@gmail.com>
2026-04-02 17:35:57 +0300

    Day 1 assignment

--- day-1/main.cpp
+++ day-1/main.cpp
@@ -2,9 +2,6 @@
 
 #include "LinkedList.h"
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
 int main() {
     LinkedList ll;
     ll.push_front(10);
