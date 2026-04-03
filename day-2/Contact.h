e98fe572ad74f5dd58421cf4d0b4d37db8a7daea
Unicorn7141 <harelibr@gmail.com>
2026-04-03 22:13:22 +0300

    Day 2 assignment

---
+++ day-2/Contact.h
@@ -0,0 +1,14 @@
+//
+// Created by harel on 02/04/2026.
+//
+
+#ifndef DAY_2_CONTACT_H
+#define DAY_2_CONTACT_H
+#include <string>
+
+struct Contact {
+    std::string name;
+    std::string phone;
+    std::string email;
+};
+#endif //DAY_2_CONTACT_H
---
+++ day-2/main.cpp
@@ -0,0 +1,93 @@
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <unordered_map>
+#include <limits>
+
+#include "Contact.h"
+
+int main() {
+    std::unordered_map<std::string, Contact> contacts;
+    std::string keyword;
+    int action;
+    std::string name, phone, email;
+
+    while (true) {
+        // TIP Select an option
+        std::cout << "Choose an option: \n";
+        std::cout << "\t 1. Add contact\n";
+        std::cout << "\t 2. Remove contact\n";
+        std::cout << "\t 3. View contacts\n";
+        std::cout << "\t 4. Exit\n";
+        std::cout << "\nAction [1~4]: ";
+        std::cin >> action;
+
+        if (action < 1 || action > 4) {
+            std::cout << "\nInvalid action. Try again.\n";
+            continue;
+        } else {
+            if (action == 4) {
+                std::cout << "EXITING" << std::endl;
+                exit(0);
+            } else if (action == 1) {
+                // TIP Ignore the <code>'\n'</code> from the action selection
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                // TIP Add a contact to the list
+                std::cout << "\nAdd contact\n";
+                std::cout << "Name: ";
+                std::getline(std::cin, name);
+                std::cout << "Phone: ";
+                std::getline(std::cin, phone);
+                std::cout << "Email: ";
+                std::getline(std::cin, email);
+                auto c = Contact(name, phone, email);
+                contacts.insert(std::make_pair(name, c));
+            } else if (action == 2) {
+                std::cout << "\nRemove contact\n";
+                std::cout << "Name: ";
+                std::cin >> name;
+                if (contacts.contains(name)) {contacts.erase(name);}
+                else { std::cout << "No contact named " << name << " was found!\n" << std::endl;}
+            }
+            else {
+                std::cout << "\n--- Phone Book ---\n";
+                int emailSize = 1;
+                int nameSize = 1;
+                int phoneSize = 1;
+
+                for (auto &contact: contacts) {
+                    if (contact.second.phone.length() > phoneSize)
+                        phoneSize = static_cast<int>(contact.second.phone.length());
+                    if (contact.second.email.length() > emailSize)
+                        emailSize = static_cast<int>(contact.second.email.length());
+                    if (contact.second.name.length() > nameSize)
+                        nameSize = static_cast<int>(contact.second.name.length());
+                }
+                // TIP Header
+                std::cout << std::left << std::setw(++nameSize) << "Name"
+                        << std::setw(++phoneSize) << "Phone"
+                        << "Email" << "\n";
+
+                for (const auto &pair: contacts) {
+                    const Contact &c = pair.second;
+                    std::cout << std::left << std::setw(nameSize) << c.name
+                            << std::setw(phoneSize) << c.phone
+                            << c.email << "\n";
+                }
+            }
+        }
+
+        // TIP Find the first match using <code>std::find_if</code>
+        auto it = std::find_if(contacts.begin(), contacts.end(), [keyword](const auto &pair) {
+            return pair.first.find(keyword) != std::string::npos;
+        });
+
+        if (it != contacts.end()) {
+            continue;
+        }
+        std::cout << "No such contact: " << keyword << "\n";
+
+        std::cout << std::endl;
+    }
+    return 0;
+}
