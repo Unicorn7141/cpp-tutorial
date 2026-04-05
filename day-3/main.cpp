c2f60f20a032a22a54530fdd90b80e9f2a730975
Unicorn7141 <harelibr@gmail.com>
2026-04-05 22:56:33 +0300

    Day 3 assignment fixes

--- day-3/main.cpp
+++ day-3/main.cpp
@@ -8,18 +8,20 @@ int main() {
     int cnt = 0;
 
     auto logger = [](const float val) { std::cout << "[LOG] New value registered: " << val << "\n"; };
-    auto counter = [&cnt](float) { cnt++; };;
+    auto counter = [&cnt](int) { cnt++; };;
     auto alert = [](const float val) {
         if (val > 100.0f) std::cout << "[ALERT] Value exceeds the threshold!\n";
     };
 
-    const auto id1 = bus.subscribe<float>("sensor_data", logger);
-    const auto id2 = bus.subscribe<float>("sensor_data", counter);
-    const auto id3 = bus.subscribe<float>("sensor_data", alert);
+    bus.subscribe<float>("sensor_data", logger);
+    bus.subscribe<int>("sensor_data", counter);
+    const ListenerID alertId = bus.subscribe<float>("sensor_data", alert);
+
+
 
     bus.emit("sensor_data", 90.0f);
     bus.emit("sensor_data", 109.8f);
-    bus.unsubscribe("sensor_data", id3);
+    bus.unsubscribe("sensor_data", alertId);
     cnt--;
     bus.emit("sensor_data", 112.3f);
     std::cout << "Listeners: " << cnt << std::endl;
